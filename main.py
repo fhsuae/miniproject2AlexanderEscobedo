b64239b98a92bae82c5f9bf201b7435bd37e9879
fhsuae <a_escobedo2@mail.fhsu.edu>
2025-09-21 02:26:24 -0500

    Updated requirements.txt

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 ### Mini Project 2
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_excel("./data/IMDB_processed_data.xlsx", index_col=0)
 
-print(df[['Ratings']].head())
+ratings = df[['Ratings']]
+
+print(ratings.count())
 
 # This project will be using Pandas dataframes. This isn't intended to be full blown data science project. The goal here is to come up with some question and then see what API or datasets you can use to get the information needed to answer that question. This will get you familar with working with datasets and asking questions, researching APIs and gathering datasets. If you get stuck here, please email me!
 #
